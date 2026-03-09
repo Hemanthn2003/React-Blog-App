23c5a815202b6e311f6abbeb4ad456c1b0e754e2
Hemanth N <hemanthn270@gmail.com>
2026-03-09 18:05:11 +0530

    Added a planned css and used prop drilling and added header and created a search form and navigation buttons and added and displayed some blog posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header.jsx'
 import Nav from './Nav'
@@ -10,19 +8,60 @@ import NewPost from './NewPost'
 import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
-import { Route, Switch, useHistory } from 'react-router-dom';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [posts, setPosts] = useState([ {
+      id: 1,
+      title: "My Girlfriend",
+      datetime: "March 16, 2025 11:11:11 AM",
+      body: " It's my girlfriends birthday!"
+    },
+    {
+      id: 2,
+      title: "My name is Hemanth",
+      datetime: "June 04, 2026 11:17:36 AM",
+      body: "It's My Birthday"
+    },
+    {
+      id: 3,
+      title: "My Mother",
+      datetime: "July 23, 2026 11:17:36 AM",
+      body: "It's my Mothers Birtday"
+    },
+    {
+      id: 4,
+      title: "My Father",
+      datetime: "Dec 29, 2026 11:17:36 AM",
+      body: "My Father's Birthday"
+    }
+    ])
+  const [search, setSearch] = useState('')
+  const [searchResult, setSearchResult] = useState([])
   return (
-  
-      <div className='App'>
+    <div className='App'>
 
+      <Header title="Hemanth Blogs"/>
+      <Nav search={search} setSearch={setSearch}/>
 
-     </div>
-      
- 
+      <Routes>
+
+        <Route path="/" element={<Home posts={posts}/>} />
+
+        <Route path="/post" element={<NewPost />} />
+
+        <Route path="/post/:id" element={<PostPage />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="*" element={<Missing />} />
+
+      </Routes>
+
+      <Footer />
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-    <Route path="/" Component={App} />
+      <Routes>
+    <Route path="/*" element={<App />} />
+    </Routes>
     </Router>
   </StrictMode>,
 )
